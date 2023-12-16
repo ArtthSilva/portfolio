e396d63df144dfbaa54606d7d2218fb1857457df
ArtthSilva <arthur12pereira1@gmail.com>
2023-12-15 21:20:24 -0300

    feat: add saudação de acordo com o horário

--- src/app/modules/home/components/home-page/home-page.component.ts
+++ src/app/modules/home/components/home-page/home-page.component.ts
@@ -1,3 +1,4 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
+
+  public status: string = ""
+
+  getGreeting() {
+    const hour = new Date().getHours();
+    let greeting: string = '';
+ 
+    if (hour >= 5 && hour <= 11) {
+        greeting = 'Bom dia';
+    } else if (hour >= 12 && hour <= 18) {
+        greeting = 'Boa tarde';
+    } else if (hour >= 19 && hour <= 23) {
+        greeting = 'Boa noite';
+    } else if (hour >= 0 && hour <= 4){
+      greeting = 'Boa madrugada'
+    }
+ 
+    this.status = greeting;
+ }
+
+ 
+ ngOnInit() {
+  setInterval(() => {
+      this.getGreeting();
+  }, 1000);
+}
 
 }

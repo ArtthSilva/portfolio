bc4895620c3b6acb516f2bd9d791f68a59cd565a
ArtthSilva <arthur12pereira1@gmail.com>
2023-12-20 01:01:10 -0300

    feat: add cards de principais projetos

--- src/app/modules/home/components/project-cards/project-cards.component.ts
+++ src/app/modules/home/components/project-cards/project-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-cards',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./project-cards.component.scss']
 })
 export class ProjectCardsComponent {
+
+  @Input()
+  projectName = '';
+
+  @Input()
+  technologies = '';
 
 }

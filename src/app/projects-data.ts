7e0b84145259d9864f8885d5fefdfab679846b91
ArtthSilva <arthur12pereira1@gmail.com>
2024-01-10 11:27:17 -0300

    feat: add estilização da página de informações dos projetos

--- src/app/projects-data.ts
+++ src/app/projects-data.ts
@@ -2,6 +2,7 @@ export interface Project{
     id: number;
     title: string;
     description: string;
+    album: string;
     
 }
 
@@ -12,7 +13,8 @@ export const projects: Project[] = [
         title: 'Cardápio Digital',
         description: `Aplicação front-end interativa, proporcionando uma experiência dinâmica ao exibir itens. O usuário terá a 
         capacidade de escolher entre diferentes categorias e interagir de forma intuitiva na seleção de produtos, 
-        promovendo uma experiência envolvente e personalizada`
+        promovendo uma experiência envolvente e personalizada`,
+        album: '../../../../../assets/images/cardapio-front-album.svg'
     },
         {
         id: 2,
@@ -20,7 +22,8 @@ export const projects: Project[] = [
         description: `Aplicação mobile que combina a familiar interface do Tinder com a profunda sabedoria dos filósofos para 
         criar uma plataforma de aprendizado única. O objetivo é tornar o aprendizado da filosofia mais acessível, 
         envolvente e divertido, conectando estudantes e entusiastas da filosofia com pensadores clássicos e suas 
-        ideias`
+        ideias`,
+        album: '../../../../../assets/images/philomatch-album.svg'
     },
         {
         id: 3,
@@ -29,19 +32,22 @@ export const projects: Project[] = [
         dinâmica das informações. A implementação inclui a utilização de tecnologias como JPA para mapeamento 
         objeto-relacional, o gerenciador de projetos Maven para automação e o banco de dados PostgreSQL para 
         assegurar uma base sólida e escalável.
-        `
+        `,
+        album: '../../../../../assets/images/cardapio-back-album.svg'
     },
         {
         id: 4,
         title: 'ClimApp',
         description: `Aplicação mobile que mostra a temperatura, clima e vento atual do país solicitado utilizando a api da OpenWeatherMap
-        `
+        `,
+        album: '../../../../../assets/images/climApp-album.svg'
     },
         {
         id: 5,
         title: 'ByJampa',
         description: `site desenvolvido em grupo com o objetivo de fornecer informações detalhadas e relevantes sobre os pontos turísticos mais incríveis em João Pessoa, Brasil.
-        `
+        `,
+        album: '../../../../../assets/images/byjampa-album.svg'
     }
 
     
